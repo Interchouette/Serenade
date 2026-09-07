90d571107fbef7019070d1c07c2834b3f0f0df4c
Interchouette <484423+Interchouette@users.noreply.github.com>
2026-09-07 00:43:04 +0000

    Deploy documentation for 22354bdaf868a4668af3c6c94238d818fcd639ff

--- serenade_contracts/sidebar-items.js
+++ serenade_contracts/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["version"],"mod":["cart","category","error","order","pagination","persist_param","product","unit_of_work"],"trait":["EntityId"]};
+window.SIDEBAR_ITEMS = {"fn":["version"],"mod":["error","pagination","persist_param","unit_of_work"],"trait":["EntityId"]};
