549058e5988dfc549b1f2854d37c4290ca495d03
Interchouette <484423+Interchouette@users.noreply.github.com>
2026-09-09 01:01:33 +0000

    Deploy documentation for 47219eb3fb9fcf3914c38bbde861191d1bf98647

--- trait.impl/serenade_http/middleware/trait.Middleware.js
+++ trait.impl/serenade_http/middleware/trait.Middleware.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["serenade_security",[["impl <a class=\"trait\" href=\"serenade_http/middleware/trait.Middleware.html\" title=\"trait serenade_http::middleware::Middleware\">Middleware</a> for <a class=\"struct\" href=\"serenade_security/struct.FirewallMiddleware.html\" title=\"struct serenade_security::FirewallMiddleware\">FirewallMiddleware</a>",0]]],["serenade_translation",[["impl <a class=\"trait\" href=\"serenade_http/middleware/trait.Middleware.html\" title=\"trait serenade_http::middleware::Middleware\">Middleware</a> for <a class=\"struct\" href=\"serenade_translation/struct.LocaleMiddleware.html\" title=\"struct serenade_translation::LocaleMiddleware\">LocaleMiddleware</a>",0]]]]);
+    const implementors = Object.fromEntries([["serenade_profiler",[["impl <a class=\"trait\" href=\"serenade_http/middleware/trait.Middleware.html\" title=\"trait serenade_http::middleware::Middleware\">Middleware</a> for <a class=\"struct\" href=\"serenade_profiler/struct.ProfilerMiddleware.html\" title=\"struct serenade_profiler::ProfilerMiddleware\">ProfilerMiddleware</a>",0]]],["serenade_security",[["impl <a class=\"trait\" href=\"serenade_http/middleware/trait.Middleware.html\" title=\"trait serenade_http::middleware::Middleware\">Middleware</a> for <a class=\"struct\" href=\"serenade_security/struct.FirewallMiddleware.html\" title=\"struct serenade_security::FirewallMiddleware\">FirewallMiddleware</a>",0]]],["serenade_translation",[["impl <a class=\"trait\" href=\"serenade_http/middleware/trait.Middleware.html\" title=\"trait serenade_http::middleware::Middleware\">Middleware</a> for <a class=\"struct\" href=\"serenade_translation/struct.LocaleMiddleware.html\" title=\"struct serenade_translation::LocaleMiddleware\">LocaleMiddleware</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[338,342]}
+//{"start":59,"fragment_lengths":[338,339,342]}
---
+++ trait.impl/tracing_subscriber/layer/trait.Layer.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["serenade_profiler",[["impl&lt;S&gt; Layer&lt;S&gt; for <a class=\"struct\" href=\"serenade_profiler/struct.ProfilerLogLayer.html\" title=\"struct serenade_profiler::ProfilerLogLayer\">ProfilerLogLayer</a><div class=\"where\">where\n    S: Subscriber,</div>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[264]}
