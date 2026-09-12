a3419fe502475bec3a8187f0e8d4ea0505957d53
Interchouette <484423+Interchouette@users.noreply.github.com>
2026-09-12 22:30:17 +0000

    Deploy documentation for d4c7814c6b497eaad1ffdfcdb30c3c4de6435cfe

---
+++ search.index/7d9fca8e5f.js
@@ -0,0 +1,1 @@
+rn_("BQLBAAASaA0Ukg1lbgXAwQEiAACydg6yeQ6yfA6hwgAAZwvzggJibhUBwgAAFlwPaNkM+wJjczv9AUgCxAAADW7AAAAN7w==")
--- trait.impl/serenade_http/async_middleware/trait.AsyncMiddleware.js
+++ trait.impl/serenade_http/async_middleware/trait.AsyncMiddleware.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["serenade_profiler",[["impl <a class=\"trait\" href=\"serenade_http/async_middleware/trait.AsyncMiddleware.html\" title=\"trait serenade_http::async_middleware::AsyncMiddleware\">AsyncMiddleware</a> for <a class=\"struct\" href=\"serenade_profiler/struct.AsyncProfilerMiddleware.html\" title=\"struct serenade_profiler::AsyncProfilerMiddleware\">AsyncProfilerMiddleware</a>",0]]],["serenade_session",[["impl <a class=\"trait\" href=\"serenade_http/async_middleware/trait.AsyncMiddleware.html\" title=\"trait serenade_http::async_middleware::AsyncMiddleware\">AsyncMiddleware</a> for <a class=\"struct\" href=\"serenade_session/struct.AsyncSessionMiddleware.html\" title=\"struct serenade_session::AsyncSessionMiddleware\">AsyncSessionMiddleware</a>",0]]]]);
+    const implementors = Object.fromEntries([["serenade_profiler",[["impl <a class=\"trait\" href=\"serenade_http/async_middleware/trait.AsyncMiddleware.html\" title=\"trait serenade_http::async_middleware::AsyncMiddleware\">AsyncMiddleware</a> for <a class=\"struct\" href=\"serenade_profiler/struct.AsyncProfilerMiddleware.html\" title=\"struct serenade_profiler::AsyncProfilerMiddleware\">AsyncProfilerMiddleware</a>",0]]],["serenade_security",[["impl <a class=\"trait\" href=\"serenade_http/async_middleware/trait.AsyncMiddleware.html\" title=\"trait serenade_http::async_middleware::AsyncMiddleware\">AsyncMiddleware</a> for <a class=\"struct\" href=\"serenade_security/struct.AsyncSessionTokenMiddleware.html\" title=\"struct serenade_security::AsyncSessionTokenMiddleware\">AsyncSessionTokenMiddleware</a>",0]]],["serenade_session",[["impl <a class=\"trait\" href=\"serenade_http/async_middleware/trait.AsyncMiddleware.html\" title=\"trait serenade_http::async_middleware::AsyncMiddleware\">AsyncMiddleware</a> for <a class=\"struct\" href=\"serenade_session/struct.AsyncSessionMiddleware.html\" title=\"struct serenade_session::AsyncSessionMiddleware\">AsyncSessionMiddleware</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[380,375]}
+//{"start":59,"fragment_lengths":[380,393,375]}
