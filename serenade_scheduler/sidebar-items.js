8320a8441a97d19d3c6d6e9cedbadec63f3c2489
Interchouette <484423+Interchouette@users.noreply.github.com>
2026-09-14 09:45:35 +0000

    Deploy documentation for 78a2c843ffb6fe267a7a1eb713a1ee1b19aabea5

--- serenade_scheduler/sidebar-items.js
+++ serenade_scheduler/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["SchedulerError","Trigger"],"fn":["validate_id","version"],"struct":["DueJob","ManualClock","Schedule","Scheduler","SystemClock"],"trait":["Clock"]};
+window.SIDEBAR_ITEMS = {"constant":["DEFAULT_SCHEDULER_SERVICE","SCHEDULER_TAG"],"enum":["SchedulerError","Trigger"],"fn":["validate_id","version"],"struct":["CommandOnDue","DueJob","FnScheduleHandler","HandlerMap","ManualClock","RegisterDefaultSchedulerPass","RunSchedulerCommand","Schedule","Scheduler","SchedulerService","SystemClock"],"trait":["Clock","ScheduleHandler"]};
