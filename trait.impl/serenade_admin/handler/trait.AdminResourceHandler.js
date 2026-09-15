2063962802dd1642606c6c40c342977f8113e510
Interchouette <484423+Interchouette@users.noreply.github.com>
2026-09-15 22:16:42 +0000

    Deploy documentation for bf59b090ffae981ced39410ccac17d98c33b663b

--- trait.impl/serenade_admin/handler/trait.AdminResourceHandler.js
+++ trait.impl/serenade_admin/handler/trait.AdminResourceHandler.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["myfeed",[["impl <a class=\"trait\" href=\"serenade_admin/handler/trait.AdminResourceHandler.html\" title=\"trait serenade_admin::handler::AdminResourceHandler\">AdminResourceHandler</a> for <a class=\"struct\" href=\"myfeed/admin_crud/struct.CategoryHandler.html\" title=\"struct myfeed::admin_crud::CategoryHandler\">CategoryHandler</a>",0]]]]);
+    const implementors = Object.fromEntries([["myfeed",[["impl AdminResourceHandler for <a class=\"struct\" href=\"myfeed/admin_crud/struct.CategoryHandler.html\" title=\"struct myfeed::admin_crud::CategoryHandler\">CategoryHandler</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[345]}
+//{"start":59,"fragment_lengths":[196]}
